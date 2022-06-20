88e554ccc3ef510a94b3b3c5a866dbfb65a7ee5b
DakshHinger <78601608+DakshHinger@users.noreply.github.com>
2022-06-20 22:17:39 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+    // console.log(firstName,lastName,email);
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us18.api.mailchimp.com/3.0/lists/eee813b366";
+
+    const options = {
+        method: "POST",
+        auth: "dakshS:c17da8d0cf7dcb7a74f574d0d9aa32cc-us18"
+    }
+
+    const request = https.request(url, options, function (response) {
+        if (response.statusCode == 200) {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+
+
+});
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("http://localhost:3000");
+});
+
+// API Key
+// c17da8d0cf7dcb7a74f574d0d9aa32cc-us18
+
+// audience id
+// eee813b366
